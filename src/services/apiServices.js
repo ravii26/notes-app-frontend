e007062986bd50cbf6d1b13ef3ed7da4094bb79e
Ravindra Zala <ravindra@kriyaninfotech.com>
2025-04-03 11:49:45 +0530

    update API_BASE_URL to use environment variable for backend URL

--- src/services/apiServices.js
+++ src/services/apiServices.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5000/api/v1";
+// const API_BASE_URL = "http://localhost:5000/api/v1";
+const API_BASE_URL = process.env.REACT_APP_BACKEND_URL || "http://localhost:5000/api/v1";
 
 const getToken = () => localStorage.getItem("token");
 
